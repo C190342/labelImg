c59a99bbd06d186c8656ec6f8fb8d325163b9a12
PHAM THI ANH <51303885+C190342@users.noreply.github.com>
2022-02-15 16:25:28 +0900

    Update resize-images.py

--- converters/resize-images.py
+++ converters/resize-images.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import os
 from PIL import Image
 import sys
@@ -22,31 +16,40 @@ max_size = int(input("Enter the max pixel size of the image (ex: 1024): "))
 
 folder_for_resized_images = "new_" + str(max_size) + "_images"
 os.chdir(folder_holding_orig_images)
-os.mkdir(folder_for_resized_images)
 
-print("Updating image sizes, this may take a while, enjoy some quiet time while you wait...")
-for each_image_file in os.listdir(folder_holding_orig_images):
-  if each_image_file.endswith("jpg") or each_image_file.endswith("jpeg") or each_image_file.endswith("png"):
-    orig_img = Image.open(each_image_file)
-    new_file_path = os.path.join(folder_for_resized_images, each_image_file)
-    aspect_ratio = orig_img.width / orig_img.height
+link_create = folder_holding_orig_images + os.sep + folder_for_resized_images
 
-    if orig_img.width > orig_img.height:
-      new_width = max_size
-      new_height = int(new_width / aspect_ratio)
-    if orig_img.height > orig_img.width:
-      new_height = max_size
-      new_width = int(new_height * aspect_ratio)
+def yes_no_input(max_size,link_create):
+    while True:
+        choice = input("Images with size of " + str(max_size)+"px will be to create in "+ link_create +" folder. 'yes' or 'no' [y/N]: ").lower()
+        if choice in ['y', 'ye', 'yes']:
+              return True
+        elif choice in ['n', 'no']:
+            return False
+        
+if yes_no_input(max_size,link_create):
+    if not os.path.exists(folder_holding_orig_images + os.sep + folder_for_resized_images):
+        # If an XML folder does not already exist, make one
+        os.mkdir(folder_for_resized_images)
 
-    new_image = orig_img.resize((new_width, new_height))
+    print("Updating image sizes, this may take a while, enjoy some quiet time while you wait...")
+    for each_image_file in os.listdir(folder_holding_orig_images):
+        if each_image_file.endswith("jpg") or each_image_file.endswith("jpeg") or each_image_file.endswith("png"):
+            orig_img = Image.open(each_image_file)
+            new_file_path = os.path.join(folder_for_resized_images, each_image_file)
+            aspect_ratio = orig_img.width / orig_img.height
 
-    new_image.save(new_file_path)
+            if orig_img.width > orig_img.height:
+                new_width = max_size
+                new_height = int(new_width / aspect_ratio)
+            if orig_img.height > orig_img.width:
+                new_height = max_size
+                new_width = int(new_height * aspect_ratio)
 
-print("Image size update complete")
+            new_image = orig_img.resize((new_width, new_height))
 
+            new_image.save(new_file_path)
 
-# In[ ]:
-
-
-
-
+    print("Image size update complete in: " + folder_holding_orig_images + os.sep + folder_for_resized_images+" folder.")
+else:
+    print("Application has stopped working without re-size image.")
